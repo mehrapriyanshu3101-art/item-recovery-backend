a174ae6c77b9e7d02b12f2f8521b982054e28c2d
mehrapriyanshu3101-art <mehrapriyanshu3101@gmail.com>
2026-04-29 13:28:43 +0530

    Update UserRepository.java

--- src/main/java/com/mehra/recovery/UserRepository.java
+++ src/main/java/com/mehra/recovery/UserRepository.java
@@ -2,8 +2,8 @@ package com.mehra.recovery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUniqueToken(String uniqueToken);
-    Optional<User> findByEmail(String email);
-}
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    // This allows UserController to find owners via QR token
+    Optional<User> findByUniqueToken(String uniqueToken);
+}
