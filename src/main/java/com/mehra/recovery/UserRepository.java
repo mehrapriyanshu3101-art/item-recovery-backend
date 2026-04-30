18347dc38d0f271f36d7ccd8ee1f04f16e53ad37
mehrapriyanshu3101-art <mehrapriyanshu3101@gmail.com>
2026-04-30 21:26:23 +0530

    Delete src/main/java/com/mehra/recovery directory

--- src/main/java/com/mehra/recovery/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mehra.foundit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // This allows UserController to find owners via QR token
-    Optional<User> findByUniqueToken(String uniqueToken);
-}
